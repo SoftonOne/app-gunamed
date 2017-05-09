4d5dac24427dbaad8965945592e0814b0156e08d
jhopes <jhopes.p@gmail.com>
2017-05-08 19:14:26 -0500

    Quitando implementacion del APT

--- model/Usuario.php
+++ model/Usuario.php
@@ -1,5 +1,11 @@
 <?php
 	require "Conexion.php";
+// patron front controler
+/**
+ * Front controller - routes all request and dispatch it to modules.
+ * @package FrameWork
+ * @subpackage FrontController
+ */
 
 	class usuario{
 	
